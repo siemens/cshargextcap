379e4c592da01c7084eaa39773d48ca945a858ef
thediveo <thediveo@gmx.eu>
2023-09-22 10:20:36 +0200

    refactor: convert to git-based source build

--- packaging/aur/packagebuildandtest.sh
+++ packaging/aur/packagebuildandtest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 cp -r /pkg /tmp/pkg
 cd /tmp/pkg
 
-makepkg -g >> PKGBUILD
 makepkg --noconfirm --syncdeps --rmdeps --clean --install
 
 # Pass back the PKGBUILD with checksum(s) included so that we can publish it as
@@ -15,10 +18,10 @@ cp PKGBUILD /pkg/dist/
 # Ask tshark to tell us the extcap interfaces it knows of: this must list the
 # packetflix extcap so we know we've installed the plugin properly.
 tshark -D | grep packetflix \
-    && echo "OK: tshark detects extcap plugin" \
-    || (echo "FAIL: tshark doesn't detect the packetflix extcap"; exit 1)
+    && echo -e "${GREEN}OK:${NOCOLOR} tshark detects extcap plugin" \
+    || (echo -e "${RED}FAIL:${NOCOLOR} tshark doesn't detect the packetflix extcap"; exit 1)
 
 # Check that the default URL scheme handler registration is in place.
 xdg-mime query default x-scheme-handler/packetflix | grep "packetflix.desktop" \
-    && echo "OK: packetflix URL scheme handler registered" \
-    || (echo "packetflix URL scheme handler not detected"; exit 1)
+    && echo -e "${GREEN}OK:${NOCOLOR} packetflix URL scheme handler registered" \
+    || (echo -e "${RED}FAIL:${NOCOLOR} packetflix URL scheme handler not detected"; exit 1)
