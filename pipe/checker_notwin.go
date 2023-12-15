7fa651f2212f61318f13e15996e5616a00eaa799
thediveo <thediveo@gmx.eu>
2023-12-15 16:57:45 +0100

    wip: refactor to use poll
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- pipe/checker_notwin.go
+++ pipe/checker_notwin.go
@@ -20,25 +20,30 @@ import (
 // This implementation leverages [syscall.Select].
 func WaitTillBreak(fifo *os.File) {
 	log.Debug("constantly monitoring packet capture fifo status...")
-	fds := unix.FdSet{}
-	fifoFdNo := int(fifo.Fd())
+	fds := []unix.PollFd{
+		{
+			Fd:     int32(fifo.Fd()),
+			Events: unix.POLLIN + unix.POLLERR,
+		},
+	}
 	for {
 		// Check the fifo becomming readable, which signals that it has been
 		// closed. In this case, ex-termi-nate ;) Oh, and remember to correctly
 		// initialize the fdset each time before calling select() ... well, just
 		// because that's a good idea to do. :(
-		fds.Zero()
-		fds.Set(fifoFdNo)
-		_, err := unix.Select(
-			fifoFdNo+1,     // highest fd is our file descriptor.
-			&fds, nil, nil, // only watch readable.
-			nil, // no timeout, ever.
-		)
+		n, err := unix.Poll(fds, 1000 /*ms*/)
 		if err != nil {
+			if err == unix.EINTR {
+				continue
+			}
 			log.Debugf("capture fifo broken, reason: %s", err.Error())
 			return
 		}
-		if fds.IsSet(fifoFdNo) {
+		if n <= 0 {
+			continue
+		}
+		log.Debugf("poll: %+v", fds)
+		if fds[0].Revents&unix.POLLERR != 0 {
 			// Either the pipe was broken by Wireshark, or we did break it on
 			// purpose in the piping process. Anyway, we're done.
 			log.Debug("capture fifo broken, stopped monitoring.")
