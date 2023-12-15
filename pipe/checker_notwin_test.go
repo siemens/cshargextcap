135fd8d35a2f0520cc11542e3da2b5914a3571d1
thediveo <thediveo@gmx.eu>
2023-12-15 13:28:40 +0100

    fix: pipe breakage test
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- pipe/checker_notwin_test.go
+++ pipe/checker_notwin_test.go
@@ -15,17 +15,22 @@ import (
 
 var _ = Describe("pipes", func() {
 
-	It("detects when a pipe breaks", func() {
+	It("detects on the write end when a pipe breaks", func() {
+		// As Wireshark uses a named pipe it passes an extcap its name (path)
+		// and then expects the extcap to open this named pipe for writing
+		// packet capture data into it. For this test we simulate Wireshark
+		// closing its reading end and we must properly detect this situation on
+		// our writing end of the pipe.
 		r, w := Successful2R(os.Pipe())
-		defer r.Close()
+		defer w.Close()
 		go func() {
 			GinkgoHelper()
 			time.Sleep(2 * time.Second)
-			Expect(w.Close()).To(Succeed())
+			Expect(r.Close()).To(Succeed())
 		}()
 		start := time.Now()
-		WaitTillBreak(r)
-		Expect(time.Since(start).Milliseconds()).To(BeNumerically(">", 1500))
+		WaitTillBreak(w)
+		Expect(time.Since(start).Milliseconds()).To(BeNumerically(">", 1900))
 	})
 
 })
